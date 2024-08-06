bcb4414d0cb2d7508d12c19287103671551d9218
mongcheng <46275621+MongMong11@users.noreply.github.com>
2024-08-07 00:20:29 +0800

    Update Dataset preparation & selection example code.R

--- Dataset preparation & selection example code.R
+++ Dataset preparation & selection example code.R
@@ -1,111 +1,53 @@
+#library
 library(data.table)
+library(tidyverse)
 
 #import raw data
-Taiwan_Moth_data<- data.table::fread('C:/Moth/Thermal_bundance_shape/Moth Raw data/Taiwan_Moth_data.csv')
-China_Moth_data<- data.table::fread('C:/Moth/Thermal_bundance_shape/Moth Raw data/China_Moth_data.csv')
-Malaysia_Moth_data<- data.table::fread('C:/Moth/Thermal_bundance_shape/Moth Raw data/Malaysia_Moth_data.csv')
+Taiwan_Moth_data<- data.table::fread("Moth Raw data/Taiwan_Moth_data.csv",encoding = "UTF-8", colClasses = "character")
+China_Moth_data<- data.table::fread("Moth Raw data/China_Moth_data.csv",encoding = "UTF-8", colClasses = "character")
+Malaysia_Moth_data<- data.table::fread("Moth Raw data/Malaysia_Moth_data.csv",encoding = "UTF-8", colClasses = "character")
 
-
-dt1<-Taiwan_Moth_data[,Dry_Weight,by=Species]
-dt2<-China_Moth_data[,Dry_Weight,by=Species]
-dt3<-Malaysia_Moth_data[,Dry_Weight,by=Species]
-
-data<-rbind(dt1, dt2, dt3)
-length(unique(data$Species))
-
-#check raw datas' altitude orders
-s1<-unique(Taiwan_Moth_data$Altitude)
-s2<-unique(China_Moth_data$Altitude)
-s3<-unique(Malaysia_Moth_data$Altitude)
-ss1<-scale(s1)
-ss2<-scale(s2)
-ss3<-scale(s3)
-
-length(unique(Taiwan_Moth_data$Altitude))
-length(unique(China_Moth_data$Altitude))
-length(unique(Malaysia_Moth_data$Altitude))
-
-#calculate species, sites-China
-df<-China_Moth_data
-test<-df
-ss<-unique(test$Species)
-
-# table: species, number of observed sites, total individuals
-table<-data.frame(matrix(NA,length(ss),3))
-colnames(table)<-c("species","number of observered sites","total individuals")
-table[,1]<-ss
-
-
-#for loop--fill the table's information
-for (i in 1:length(ss)) {
-  num<-nrow(unique(test[which(test$Species==ss[i]),8]))
-  num2<-nrow(test[which(test$Species==ss[i]),3])
-  table[i,2]<-num
-  table[i,3]<-num2
-}
-
-library(data.table)
-
-#select data--number of site more than 3 sites; individuals more than 
-table<-data.table(table)
-table2<-table[`number of observered sites`>=3]
-table2<-table2[`total individuals`>=10]
-
-names(table2)[1]<-"Species"
-ss2<-table2$Species
+#taiwan_dt:species, number of observed sites, total individuals
+taiwan_dt<-Taiwan_Moth_data %>%
+  setDT() %>%
+  .[, .(, number_of_observered_sites := unique(altitude),
+        total_individuals := .N),
+    by = Species]
+  
+#select data--number of site more than 3 sites; individuals more than 10
+taiwan_dt_ls<-taiwan_dt %>%
+  filter(as.numeric(number_of_observered_sites)>3) %>%
+  filter(as.numeric(total_individuals)>10)
 
 #fill the datas to sort
-dt2<-China_Moth_data[df$Species %in% ss2,]
+dt_taiwan_filt<-Taiwan_Moth_data %>% left_join(taiwan_dt_ls, by = "Species")
 
+# china_dt
+china_dt<-China_Moth_data %>%
+  setDT() %>%
+  .[, .(, number_of_observered_sites := unique(altitude),
+        total_individuals := .N),
+    by = Species]
+  
+#select data--number of site more than 3 sites; individuals more than 10
+china_dt_ls<-china_dt %>%
+  filter(as.numeric(number_of_observered_sites)>3) %>%
+  filter(as.numeric(total_individuals)>10)
 
-#calculate species, sites-Taiwan
-df<-Taiwan_Moth_data
-test<-df
-ss<-unique(test$Species)
+#fill the datas to sort
+dt_china_filt<-China_Moth_data %>% left_join(china_dt_ls, by = "Species")
 
-# table: species, number of observed sites, total individuals
-table<-data.frame(matrix(NA,length(ss),3))
-colnames(table)<-c("species","number of observered sites","total individuals")
-table[,1]<-ss
+#malaysia_dt
+malaysia_dt<-Malaysia_Moth_data %>%
+  setDT() %>%
+  .[, .(, number_of_observered_sites := unique(altitude),
+        total_individuals := .N),
+    by = Species]
+  
+#select data--number of site more than 3 sites; individuals more than 10
+malaysia_dt_ls<-malaysia_dt %>%
+  filter(as.numeric(number_of_observered_sites)>3) %>%
+  filter(as.numeric(total_individuals)>10)
 
-
-for (i in 1:length(ss)) {
-  num<-nrow(unique(test[which(test$Species==ss[i]),4]))
-  num2<-nrow(test[which(test$Species==ss[i]),9])
-  table[i,2]<-num
-  table[i,3]<-num2
-}
-
-table<-data.table(table)
-table3<-table[`number of observered sites`>=3]
-table3<-table3[`total individuals`>=10]
-
-names(table3)[1]<-"Species"
-ss2<-table3$Species
-
-dt3<-Taiwan_Moth_data[df$Species %in% ss2 ,]
-
-#calculate species, sites-Malaysia
-df<-Malaysia_Moth_data
-test<-df
-ss<-unique(test$Species)
-table<-data.frame(matrix(NA,length(ss),3))
-colnames(table)<-c("species","number of observered sites","total individuals")
-table[,1]<-ss
-
-
-for (i in 1:length(ss)) {
-  num<-nrow(unique(test[which(test$Species==ss[i]),5]))
-  num2<-nrow(test[which(test$Species==ss[i]),9])
-  table[i,2]<-num
-  table[i,3]<-num2
-}
-
-table<-data.table(table)
-table4<-table[`number of observered sites`>=3]
-table4<-table4[`total individuals`>=10]
-
-names(table4)[1]<-"Species"
-ss2<-table4$Species
-
-dt4<-Malaysia_Moth_data[df$Species %in% ss2 ,]
+#fill the datas to sort
+dt_malaysia_filt<-Malaysia_Moth_data %>% left_join(malaysia_dt_ls, by = "Species")
